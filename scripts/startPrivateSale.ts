1bd9e30e106248566a4abe9cf7d7c914c83b60c3
xPainite <109281998+polarydev@users.noreply.github.com>
2022-08-03 09:43:31 -0400

    modify decimals

---
+++ scripts/startPrivateSale.ts
@@ -0,0 +1,33 @@
+import { ethers } from 'hardhat';
+import { formatUnits } from "ethers/lib/utils";
+import { PolarysNFTContract } from "../typechain-types";
+
+import 'dotenv/config';
+import { load } from "./utils"
+import {
+  getBigNumber,
+} from '../test/utils'
+
+import { Signer } from 'ethers';
+
+async function main () {
+  let signer: Signer
+  let nftContract: PolarysNFTContract
+  
+  const nftContractAddress = (await load('PolarysNFTContract')).address
+
+  nftContract = (await ethers.getContractAt("PolarysNFTContract", nftContractAddress)) as PolarysNFTContract;
+  [signer] = await ethers.getSigners()
+  
+  const transaction1 = await nftContract
+    .connect(signer).startPrivateSale();
+  await transaction1.wait();
+  
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
